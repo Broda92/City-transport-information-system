43de706b6cdde25287e76c7323503d6b79de51a1
Jan Brodský <brodsky.jan@seznam.cz>
2021-03-28 23:11:06 +0200

    Vue app created

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <RIS_II/>
+</template>
+
+<script>
+import RIS_II from './components/RIS_II.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RIS_II
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: darkgrey;
+  font-family: Arial;
+}
+</style>
